8a421a1c8a6b735e642f7d755a4ce0f783332eef
MH.Ji <battlesun99@gmail.com>
2021-06-17 15:14:27 +0900

    commit

---
+++ js/050_버블정렬구현하기.js
@@ -0,0 +1,21 @@
+function bubble(arr) {
+    let result = arr.slice();
+
+    for (let i=0; i<result.length-1; i++) {
+        for (let j=0; j<result.length-1; j++) {
+            if (result[j] > result[j+1]) {
+                var temp = result[j];
+                result[j] = result[j+1];
+                result[j+1] = temp;
+            }
+        }
+    }
+
+    return result;
+}
+
+const items = prompt("입력: ").split(" ").map((n) => {
+    return parseInt(n, 10);
+});
+
+console.log(bubble(items));
