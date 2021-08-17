c7e48d900703134c19fd5e48ee5e36499e3fc307
Ji Myung Hwa <battlesun99@gmai.com>
2021-08-17 22:12:55 +0900

    자바스크립트 Problem Solving

---
+++ src/problem_solving/__test__/퍼즐조각채우기.test.js
@@ -0,0 +1,23 @@
+const { solution } = require("../퍼즐조각채우기");
+
+const test1 = {
+    game_board: [[1,1,0,0,1,0],[0,0,1,0,1,0],[0,1,1,0,0,1],[1,1,0,1,1,1],[1,0,0,0,1,0],[0,1,1,1,0,0]],
+    table: [[1,0,0,1,1,0],[1,0,1,0,1,0],[0,1,1,0,1,1],[0,0,1,0,0,0],[1,1,0,1,1,0],[0,1,0,0,0,0]],
+    result: 14,
+};
+
+const test2 = {
+    game_board: [[0,0,0],[1,1,0],[1,1,1]],
+    table: [[1,1,1],[1,0,0],[0,0,0]],
+    result: 0,
+};
+
+describe('퍼즐 조각을 채워 넣을 경우, 총 몇 칸을 채울 수 있는지', () => {
+    test('test1', () => {
+        expect(solution(test1.game_board, test1.table)).toEqual(test1.result);
+    });
+
+    test('test2', () => {
+        expect(solution(test2.game_board, test2.table)).toEqual(test2.result);
+    });
+});
---
+++ src/problem_solving/퍼즐조각채우기.js
@@ -0,0 +1,6 @@
+function solution(game_board, table) {
+    var answer = -1;
+    return answer;
+}
+
+exports.solution = solution;
