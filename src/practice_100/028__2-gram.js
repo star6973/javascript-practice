7cff38989c108bec3d838232c8220aa59d58e178
mhji <battlesun99@gmail.com>
2021-08-18 19:10:37 +0900

    2-gram

---
+++ src/practice_100/028__2-gram.js
@@ -0,0 +1,8 @@
+function solution (st) {
+    let result = '';
+    for (let i = 0; i < str.length - 1; i++) {
+        result += `${str[i]} ${str[i+1]}` + "\n";
+    }
+}
+
+exports.solution = solution;
