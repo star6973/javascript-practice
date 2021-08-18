760f4d6ec17df4d36962468e0db4c81f80d4f134
mhji <battlesun99@gmail.com>
2021-08-18 19:10:03 +0900

    원의 넓이를 구하세요

---
+++ src/practice_100/025__원의 넓이를 구하세요.js
@@ -0,0 +1,5 @@
+function solution(n) {
+    return n * n * 3.14;
+}
+
+exports.solution = solution;
--- src/practice_100/025_원의넓이를구하세요.js
+++
@@ -1,6 +0,0 @@
-function area(n) {
-    return n * n * 3.14;
-}
-
-const n = prompt("입력: ");
-console.log(area(n));
