f739a4cb4ebefd1853eb9e38542c01386b0582b3
mhji <battlesun99@gmail.com>
2021-08-18 19:10:47 +0900

    대문자만 지나가세요

---
+++ src/practice_100/029__대문자만 지나가세요.js
@@ -0,0 +1,9 @@
+function solution(str) {
+    if (str == str.toUpperCase()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+exports.solution = solution;
--- src/practice_100/029_대문자만지나가세요.js
+++
@@ -1,7 +0,0 @@
-const str = prompt("입력: ");
-
-if (str == str.toUpperCase()) {
-    console.log("YES");
-} else {
-    console.log("NO");
-}
