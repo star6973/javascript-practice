b73d9f180db99a5042f5fdee29888d901cd8977b
mhji <battlesun99@gmail.com>
2021-08-18 19:10:55 +0900

    문자열 속 문자 찾기

---
+++ src/practice_100/030__문자열 속 문자 찾기.js
@@ -0,0 +1,6 @@
+// indexOf() 함수는주어진 값과 일치하는 값 혹은 요소의 첫 번째 인덱스를 반환하고 존재하지 않으면 -1을 반환한다.
+function solution(str, find_str) {
+    return str.indexOf(find_str);
+}
+
+exports.solution = solution;
--- src/practice_100/030_문자열속문자찾기.js
+++
@@ -1,6 +0,0 @@
-const str = prompt("입력: ");
-const find_str = prompt("입력: ");
-
-console.log(str.indexOf(find_str));
-
-// indexOf() 함수는주어진 값과 일치하는 값 혹은 요소의 첫 번째 인덱스를 반환하고 존재하지 않으면 -1을 반환한다.
